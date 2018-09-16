2d8a5357d6efdcf3e8c0e6fe983cbc60f4e29238
ashrystein <38687453+ashrystein@users.noreply.github.com>
2018-09-16 15:14:50 +0200

    RNImageMatrix works for android

--- android/src/main/java/com/reactlibrary/RNImageMatrixModule.java
+++ android/src/main/java/com/reactlibrary/RNImageMatrixModule.java
@@ -11,6 +11,20 @@ import com.facebook.react.bridge.NativeModule;
 import java.util.Map;
 import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.Arguments;
+
+import android.graphics.Bitmap;  
+import android.graphics.BitmapFactory;  
+import android.os.Bundle;  
+import android.widget.ImageView;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
+import java.io.FileOutputStream;
+
+
 ///
 public class RNImageMatrixModule extends ReactContextBaseJavaModule {
   private final ReactApplicationContext reactContext;
@@ -21,11 +35,42 @@ public class RNImageMatrixModule extends ReactContextBaseJavaModule {
   }
 
 @ReactMethod
-  public void getImageUrl(String path, int rows, int columns, Callback callback) {
+  public void getImageUrl(String path,String type,int rows, int columns, Callback callback) {
     WritableArray srcs = Arguments.createArray();
-    srcs.pushString("fileMap");
-    srcs.pushString("fileMap1");
-    callback.invoke(srcs);
+    WritableArray indces = Arguments.createArray();
+    
+    File imgFile = new  File(path);
+    if(imgFile.exists()){
+      Bitmap myBitmap = BitmapFactory.decodeFile(path);
+      int width = myBitmap.getWidth();
+      int height = myBitmap.getHeight();
+      int subImgWidth = width/columns;
+      int subImgHeight = height/rows;
+      String subImgPath = "/data/user/0/com.bizzifyevents/files/"+type; 
+      int count = 0 ;
+      
+      for(int y = 0 ; y < height-1 ; y = y+subImgHeight ){
+        for(int x = 0 ; x < width-1 ; x = x+subImgWidth ){  
+          
+          Bitmap subBitmap = Bitmap.createBitmap(myBitmap,x,y,subImgWidth,subImgHeight);
+
+          try{
+            File file = new File(subImgPath+count+".jpg");
+            file.createNewFile();
+            FileOutputStream fOut = new FileOutputStream(file);
+            subBitmap.compress(Bitmap.CompressFormat.JPEG, 100, fOut);
+            fOut.close();
+          }catch(Exception ex){
+            callback.invoke(ex);
+          }
+          srcs.pushString(subImgPath+count+".jpg");
+          count++;
+        }
+      }
+      callback.invoke(srcs);
+    }else{
+      callback.invoke(path);
+    }
   }
   
   @Override
